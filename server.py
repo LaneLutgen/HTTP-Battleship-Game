353e35d8edcd031ae3baa9cb76df0b93469acb5e
Lane Lutgen <lutgenlane@gmail.com>
2016-09-18 08:51:11 -0600

    Implemented HTTP server
    Right now it will run always, we will need to terminate somehow, perhaps
    with a timeout

--- server.py
+++ server.py
@@ -1,59 +1,62 @@
 import sys
-import http.client
 import urllib
-
-#Global variables for sink checking.
-ship_c = 5
-ship_b = 4
-ship_r = 3
-ship_s = 3
-ship_d = 1
-ship_d2 = 1
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 """
-Description: Main function for the battleship client.
-			 Takes in 2 system arguments from command line
-Input:		 sys.argv[1] - The port number
-			 sys.argv[2] - The input board file for the client (.txt)
+Description:    Main function for the battleship client.
+                Takes in 2 system arguments from command line
+Input:          sys.argv[1] - The port number
+                sys.argv[2] - The input board file for the client (.txt)
 """
+
 def main():
-	#Receive Input
-	port_num = int(sys.argv[1])
-	file_name = open(sys.argv[2], "r")
+        global ship_c
+        global ship_b
+        global ship_r 
+        global ship_s 
+        global ship_d
 
-	file_content = file_name.read()
+        #HANDLE CMD INPUT
+        port_num = int(sys.argv[1])
+        file_name = open(sys.argv[2], "r")
 
-	#TEST FUNCTION
-	test_server(port_num, file_content)
+        file_content = file_name.read()
+        
+        #TEST FUNCTION
+        test_server(port_num, file_content)
+        init_ships()
+        init_http_server(port_num)
 
+
+def init_ships():
+        ship_c = 5
+        ship_b = 4
+        ship_r = 3
+        ship_s = 3
+        ship_d = 3
 
 """
-NOT IMPLEMENTED
-
 This should be some sort of function to listen for HTTP commands from 
 the client. (May be a infinite loop but I'm not sure, would have to 
 be able to terminate it with a timeout of some sort)
 """
-def http_listener(): 
-
-"""
-NOT IMPLEMENTED
-
-This will probably be some function to grab the HTML for the player
-board so we can determine a hit, miss, sunk, etc.
-"""
-def get_player_board():
-
+def init_http_server(port_num):
+        ip_address = '127.0.0.1' #not sure if this is correct or not 
+        server_address = (ip_address, port_num)
+        httpd = HTTPServer(server_address, BattleShipHTTP_RequestHandler)
+        print("HTTP Server Initialized")
+        httpd.serve_forever()
 
 """
 Description: Main function for the battleship client.
-			 Takes in 2 system arguments from command line
-Params:		 port_num - The port number
-			 file_content - The input board file for the client (.txt)
+		Takes in 2 system arguments from command line
+Params:		port_num - The port number
+		file_content - The input board file for the client (.txt)
 """
 def test_server(port_num, file_content):
-	print(port_num)
-	print(file_content)
+        print(port_num)
+        print(file_content)
+
 
 """
 Description: Function that detects a sink.
@@ -62,24 +65,32 @@ Description: Function that detects a sink.
 """
 def check_sink():
         if ship_c == 0: #Carrier
-                return c #sends back the letter of the ship, think this will work, not sure because not good with python.
+                return 'c' #sends back the letter of the ship, think this will work, not sure because not good with python.
                 ship_c = -1 #Sets to -1, confirming that the ship has sunk, and it has been detected as such.
         if ship_b == 0: #Battleship
-                return b
+                return 'b'
                 ship_b = -1
         if ship_r == 0: #Cruiser
-                return r
+                return 'r'
                 ship_r = -1
         if ship_s == 0: #Submarine
-                return s
+                return 's'
                 ship_s = -1
         if ship_d == 0: #There are two destroyers, so we'll need a way to check which one sunk, this might work, not sure though.
-                return d
+                return 'd'
                 ship_d = -1
-        if ship_d2 == 0: 
-                return d
-                ship_d2 = -1
         return 0 #Returns a 0, for no new sinks. Not sure if this'll work.
+
+"""
+Description: Class for handling any GET messages
+"""
+class BattleShipHTTP_RequestHandler(BaseHTTPRequestHandler):
+
+        def do_GET():
+                return 0
+
+
 
 #MAIN FUNCTION CALL
 main()
+
